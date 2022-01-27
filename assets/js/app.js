89917b330411b284507279ad5d641e382ece9964
khanhhoa790 <68863404+khanhhoa790@users.noreply.github.com>
2022-01-27 13:46:02 +0700

    update menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,7 @@
 const header = document.querySelector(".header");
 const hambugerBar = document.querySelector(".menu-hamburger");
+const menu = document.querySelector(".menu");
+
+hambugerBar.addEventListener("click", function () {
+  menu.classList.toggle("menu-active");
+});
